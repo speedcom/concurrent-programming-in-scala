ca2a155cc433cb72854339d25ec51ec70862c35b
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-10-23 00:13:45 +0200

    non commutative operator

--- ch5/parallelizable_operations.scala
+++ ch5/parallelizable_operations.scala
@@ -47,3 +47,19 @@ object ParSizeEffectsCorrect extends App {
   println(s"Sequential result - $seqres")
   println(s"Parallel result - $parres")
 }
+
+object ParNonCommutativeOperator extends App {
+  val doc = mutable.ArrayBuffer.tabulate(20)(i => s"Page $i, ")
+  def test(doc: GenIterable[String]) {
+    val seqtext = doc.seq.reduceLeft(_ + _)
+    val partext = doc.par.reduce(_ + _)
+    println(s"Sequential result - $seqtext\n")
+    println(s"Parallel result - $partext\n")
+  }
+  test(doc)
+  test(doc.toSet)
+}
+
+
+
+
