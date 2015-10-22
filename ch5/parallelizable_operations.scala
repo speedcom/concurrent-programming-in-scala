7e22c76cb3f0a097a56a69195d752d6669a5f050
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-10-22 17:30:13 +0200

    show that collection which is non-parallelizable like List need to be first transformed to paralellizable collection like Vector

--- ch5/parallelizable_operations.scala
+++ ch5/parallelizable_operations.scala
@@ -4,3 +4,10 @@ def timed[T](body: =>T): Double = {
   val end = System.nanoTime
   ((end - start) / 1000) / 1000.0
 }
+
+object ParNonParallelizableCollections extends App {
+  val list = List.fill(1000000)("")
+  val vector = Vector.fill(1000000)("")
+  println(s"list conversion time: ${timed(list.par)} ms")
+  println(s"vector conversion time: ${timed(vector.par)} ms")
+}
