1a05d4d38befb0dad92d7cd16e469cf176950b8a
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-10-22 17:30:54 +0200

    incorrectly implemented intersection of two parallel-like sets

--- ch5/parallelizable_operations.scala
+++ ch5/parallelizable_operations.scala
@@ -11,3 +11,24 @@ object ParNonParallelizableCollections extends App {
   println(s"list conversion time: ${timed(list.par)} ms")
   println(s"vector conversion time: ${timed(vector.par)} ms")
 }
+
+object ParSizeEffectsIncorrect extends App {
+
+  def intersectionSize(a: GenSet[Int], b: GenSet[Int]): Int = {
+    var total = 0
+
+    for(x <- a)
+      if(b contains x)
+        total += 1
+
+    total
+  }
+
+  val a = (0 to 1000).toSet
+  val b = (0 to 1000 by 4).toSet
+
+  val seqres = intersectionSize(a, b)
+  val parres = intersectionSize(a.par, b.par)
+  println(s"Sequential result - $seqres")
+  println(s"Parallel result - $parres")
+}
