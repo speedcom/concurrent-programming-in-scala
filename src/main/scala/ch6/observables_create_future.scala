da1a8d488bec60cc5882da1a02c8b372f357d76c
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-11-21 07:39:19 +0100

    create Observable based on Future

---
+++ src/main/scala/ch6/observables_create_future.scala
@@ -0,0 +1,19 @@
+package ch6
+
+import rx.lang.scala._
+import akka.event.Logging
+import learningconcurrency._
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
+object ObservablesCreateFuture extends App {
+
+  val f = Future { "Back to the Future(s) "}
+  val o = Observable.create[String] { obs =>
+    f       .foreach { case s => obs.onNext(s); obs.onCompleted(); }
+    f.failed.foreach { case e => obs.onError(e) }
+    Subscripiton()
+  }
+  o.subscribe(log _)
+}
