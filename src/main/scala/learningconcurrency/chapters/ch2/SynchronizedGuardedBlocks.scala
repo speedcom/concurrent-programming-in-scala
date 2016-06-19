deb2cdb74b784fd4e44bffe76ba2351e7cddf6bb
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-06-19 13:42:03 +0200

    SynchronizedGuardedBlocks

---
+++ src/main/scala/learningconcurrency/chapters/ch2/SynchronizedGuardedBlocks.scala
@@ -0,0 +1,24 @@
+package learningconcurrency.chapters.ch2
+
+object SynchronizedGuardedBlocks extends App {
+
+  val lock = new AnyRef
+
+  var message: Option[String] = None
+
+  val greeter = thread {
+    lock.synchronized {
+      while (message.isEmpty)
+        lock.wait()
+      log(message.get)
+    }
+  }
+
+  lock.synchronized {
+    message = Some("Hello!")
+    lock.notify()
+  }
+
+  greeter.join()
+
+}
